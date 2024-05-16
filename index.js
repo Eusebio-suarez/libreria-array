13d60552556efadb35c15aac1f9cfbfddacab6ce
eusebio suarez <eusebiosuaresmartines@gmail.com>
2024-05-16 09:29:51 -0500

    setup inicial proyecto practica array

---
+++ index.js
@@ -0,0 +1,57 @@
+let libro = {
+titulo: "título",
+autor: "libro",
+genero: "género",
+idioma: "idioma ",
+precio: "precio",
+formato: "El formato en el que se vende el libro (por ejemplo, tapa dura, tapa blanda, ebook, etc.).",
+isbn: "número de ISBN ",
+descripcion: "descripción",
+estado: "estado del libro (por ejemplo, nuevo, usado, como nuevo, etc.).",
+ubicacion: "La ubicación donde el libro está disponible para la venta.",
+fecha_publicacion: "fecha",
+editorial: "editorial",
+paginas: "número",
+dimensiones: "dimensiones físicas",
+peso: "peso "
+}
+
+let libros = [
+    {
+        titulo: "título",
+        autor: "libro",
+        genero: "género",
+        idioma: "idioma ",
+        precio: "precio",
+        formato: "El formato en el que se vende el libro (por ejemplo, tapa dura, tapa blanda, ebook, etc.).",
+        isbn: "número de ISBN ",
+        descripcion: "descripción",
+        estado: "estado del libro (por ejemplo, nuevo, usado, como nuevo, etc.).",
+        ubicacion: "La ubicación donde el libro está disponible para la venta.",
+        fecha_publicacion: "fecha",
+        editorial:"editorial",
+        paginas: "número",
+        dimensiones:"dimensiones físicas",
+        peso: "peso "
+    },
+
+    {
+        titulo: "título",
+        autor: "libro",
+        genero: "género",
+        idioma: "idioma ",
+        precio: "precio",
+        formato: "El formato en el que se vende el libro (por ejemplo, tapa dura, tapa blanda, ebook, etc.).",
+        isbn: "número de ISBN ",
+        descripcion: "descripción",
+        estado: "estado del libro (por ejemplo, nuevo, usado, como nuevo, etc.).",
+        ubicacion: "La ubicación donde el libro está disponible para la venta.",
+        fecha_publicacion: "fecha",
+        editorial:"editorial",
+        paginas: "número",
+        dimensiones:"dimensiones físicas",
+        peso: "peso 34"
+    }
+]
+
+console.log(libros[1]);
